a1fd354cbabacae6d4c44ca5576f316b12f59ff8
Charles Kaminer <ckamine@g.clemson.edu>
2016-08-29 11:28:32 -0600

    set up app server views and tests test get request to home page setup index.jade begin writing POST test

---
+++ server.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const app = express();
+
+const path = require('path');
+
+app.use(express.static('static'));
+
+app.set('port', process.env.PORT || 3000);
+app.locals.title = 'Pizza Express';
+
+app.set('view engine', 'jade');
+
+app.locals.pizza = {};
+
+app.get('/', (request, response) => {
+  response.render('index');
+});
+
+if (!module.parent) {
+  app.listen(app.get('port'), () => {
+    console.log(`${app.locals.title} is running on ${app.get('port')}.`);
+  });
+}
+
+module.exports = app;
